fe81987417b5d629c0ae7f60ed42edc4b8e360fb
frmauro <frmauro8@gmail.com>
2025-03-21 15:19:04 -0300

    many changes

--- dist/components/Footer.js
+++
@@ -1,3 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-const Footer = () => (_jsx("footer", Object.assign({ style: { background: "#333", color: "#fff", padding: "10px", textAlign: "center" } }, { children: _jsx("p", { children: "\u00A9 2025 Minha Aplica\u00E7\u00E3o" }) })));
-export default Footer;
--- dist/components/Sidebar.js
+++
@@ -1,4 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import { Link } from "react-router-dom";
-const Sidebar = () => (_jsx("nav", Object.assign({ style: { width: "200px", background: "#ddd", padding: "10px" } }, { children: _jsxs("ul", Object.assign({ style: { listStyle: "none", padding: 0 } }, { children: [_jsx("li", { children: _jsx(Link, Object.assign({ to: "/home" }, { children: "Home" })) }), _jsx("li", { children: _jsx(Link, Object.assign({ to: "/products" }, { children: "Products" })) }), _jsx("li", { children: _jsx(Link, Object.assign({ to: "/login" }, { children: "Login" })) })] })) })));
-export default Sidebar;
